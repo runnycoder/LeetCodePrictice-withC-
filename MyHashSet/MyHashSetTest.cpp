1eff4c69980acd485e5c24c94da349bad6913b41
runny <842655705@qq.com>
2020-07-22 22:32:31 +0800

    HashSet测试

--- MyHashSet/MyHashSetTest.cpp
+++ MyHashSet/MyHashSetTest.cpp
@@ -1,6 +1,33 @@
 #include<iostream>
+#include<unordered_set>
 using namespace std;
 int main(){
     cout<<"git merge dev_test branch to master branch"<<endl;
+    //initialize a has set
+    unordered_set<int> hashset;
+    //insert a new key
+    hashset.insert(3);
+    hashset.insert(2);
+    hashset.insert(1);
+    //delete a key
+    hashset.erase(2);
+    //check if the key is in the hashset
+    if(hashset.count(2)<=0){
+        cout<<"key 2 is not in the hashset."<<endl;
+    }
+    //get the size of hashset
+    cout<<"this hashset size is "<<hashset.size()<<endl;
+
+    for (auto it = hashset.begin(); it != hashset.end(); it++)
+    {
+        /* code */
+        cout<<(*it)<<" ";
+    }
+    cout<<"are int the hashset"<<endl;
+
+    hashset.clear();
+    if(hashset.empty()){
+        cout<<"hashset is empty"<<endl;
+    }
     return 0;
 }
